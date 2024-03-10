1858884acf969ecc29cb9516d333e2db6db948b4
Rokade03 <aayushrokade03@gmail.com>
2024-03-11 00:49:35 +0530

    Linked Cards to their official websites

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,9 +5,9 @@ function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
+        <a href={props.anchor} className='cards__item__link'>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
+          <img 
               className='cards__item__img'
               alt='Travel Image'
               src={props.src}
@@ -17,7 +17,7 @@ function CardItem(props) {
             <h5 className='cards__item__text'>{props.text}</h5>
             <p className='cards__item__paragraph'>{props.paragraph}</p>
           </div>
-        </Link>
+        </a>
       </li>
     </>
   );
--- src/components/Club_Cards.js
+++ src/components/Club_Cards.js
@@ -8,40 +8,40 @@ function ClubCards() {
       <h1>Clubs You Can See</h1>
       <div className='club_cards_container'>
           <ul className='club_cards_items'>
-            <CardItem
+           <CardItem
               src='images/img-9.jpg'
               text='Google Developer Student Club'
               paragraph='This is GDSC'
               label='Technical'
-              path='/'
+              anchor='https://gdsc.community.dev/ap-shah-institute-of-technology-thane/'
             />
             <CardItem
               src='images/img-2.jpg'
               text='Institute of Electrical and Electronics Engineers(IEEE)'
               paragraph='This is IEEE'
               label='Technical'
-              path='/'
+              anchor='https://ieee.eventsapsit.org/'
             />
             <CardItem
               src='images/img-3.jpg'
               text='OJUS'
               paragraph='This is OJUS'
               label='Sports/Cultural'
-              path='/services'
+              anchor='https://www.linkedin.com/company/ojus-apsit/about/'
             />
             <CardItem
               src='images/img-4.jpg'
               text='Antarang'
               paragraph='This is Antarang'
               label='Health'
-              path='/products'
+              anchor='https://www.facebook.com/apsitantarang/'
             />
             <CardItem
               src='images/img-8.jpg'
               text='A.P. Shah Institute Of Technology'
               paragraph='This is APSIT'
               label='College'
-              path='/sign-up'
+              anchor='http://moodle.apsit.org.in/moodle/'
             />
           </ul>
         </div>
