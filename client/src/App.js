c237f943068077b1bdca472cec005254e636d849
Rokade03 <aayushrokade03@gmail.com>
2024-04-12 01:44:00 +0530

    Project Destruction

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Register_Events from './components/pages/Register_Events.js';
 import Events_Display from './components/pages/Events_Display.js';
 import Email_Extract from './components/pages/Email_Extract.js';
 import Registration_Select from './components/pages/Registation_Select.js';
-import EmailExtractor from './components/EmailExtractor.js';
+import Register_Student from './components/Register_Student.js';
 // import { Switch } from 'react-router-dom';
 
 function App() {
@@ -29,6 +29,7 @@ function App() {
           <Route path='/Organize' element={<Organize/>}/>  
           <Route path='/Login' element={<Login/>}/> 
           <Route path='/Register' element={<Register/>}/>
+          <Route path='/Register_Student' element={<Register_Student/>}/> 
           <Route path='/Register_Events' element={<Register_Events/>}/> 
           <Route path='/Events_Display' element={<Events_Display/>}/> 
           <Route path='/Email_Extract' element={<Email_Extract/>}/> 
